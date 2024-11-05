0b78e5b2839a691682feae606172433e12e9d252
Micha Hesemans <micha_hesemans@hotmail.com>
2024-11-05 12:03:00 +0000

    feat: added footer

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    //sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+});
