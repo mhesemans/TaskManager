9b0765ac6a2d842323ab2ffe2914004039d61d41
Micha Hesemans <micha_hesemans@hotmail.com>
2024-11-14 01:59:23 +0000

    feat: added add_task template and function to create tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // date picker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // Select Initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
